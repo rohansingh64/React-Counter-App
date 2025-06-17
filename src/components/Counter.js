ceaa94fb9f619836746b8ecb4fc8288d7452c5b0
rohansingh64 <rohansinghrohansingh64@gmail.com>
2025-06-18 01:40:18 +0530

    //

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,50 @@ import './Counter.css';
 
 function Counter(){
 
+
+
+    // when you use usestate  -->  when there is change in data and want to new data reflects on UI also
+
+
+
+    // useState is a React Hook that lets you add state (i.e., data that changes over time) to functional components.
+
+    // syntax -->  let [varName,changeValueFunction] = useState(initialValue);
+
+    //   changeValueFunction(newValue);
+
+    //  ------    useState  not change value immediataly  --> it schedule change then render new value on UI
+
+
+
+
+
+    // we use useState for rendering again the UI content after change in i variable
+
+
+
+    //************************************************** */
+    // if we do not use useState --> then if we change in i then i value changed [verify by clg(i)] but not render on UI
+
+
+
+
+
+
+    //  alternative of eventlistner(of js) in react --->
+
+    //  event --> click event --> how to add click event --> use onClick = {Handlerfn} as props <------**************#######
+
+    // change in input  ---> onChange={Handlerfnfn} as props
+
+    // submit  ---> onSubmit={Handlerfnfn} as props
+
+
+
+
+
+
+
     let [i,changeIvalue] = useState(0);
 
 
@@ -24,6 +68,12 @@ function Counter(){
 
         <div className='outerContainer'>
 
+            <div>
+                <span>Increment</span>
+                <span>  &  </span>
+                <span>Decrement</span>
+            </div>
+
             <div className='Container'>
                 <div><button onClick={decreaseCounterHandler}> - </button></div>
                 <p>counter = {i} </p>
@@ -31,7 +81,7 @@ function Counter(){
             </div>
 
             <div>
-                <button onClick={refreshCounterHandler} className='refreshbutton'>refresh counter</button>
+                <button onClick={refreshCounterHandler} className='refreshbutton'>reset</button>
             </div>
 
         </div>
